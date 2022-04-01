ad3a6785a1f880e698033fc551097b862e68bc43
boris_tchouanfe <borisfeze9@gmail.com>
2022-04-01 15:56:18 +0100

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,389 @@
+<template>
+  <section class="top-section">
+    <div class="container-lg py-5">
+      <div class="container text-center d-block d-sm-none">
+        <img src="../assets/images/p3.png" class="smprofile-img" alt="..." />
+      </div>
+      <div class="row justify-content-center align-items-center">
+        <div class="col-xs-10 col-sm-10 col-md-6 col-lg-7">
+          <div class="container my-2">
+            <p class="text-xs-center fs-5">Hello, I'm called</p>
+            <p class="display-5 text-xs-center fw-bold">BORIS TCHOUANFE</p>
+            <p class="text-xs-center fs-5">Web Developer</p>
+            <div class="btn-container">
+              <button type="button" class="btn cv-btn">download cv</button>
+
+              <button type="button" class="btn btn-outline play-btn">
+                play video
+              </button>
+            </div>
+          </div>
+        </div>
+        <div
+          class="
+            col-xs-10 col-sm-10 col-md-6 col-lg-5
+            d-sm-none d-md-block d-none d-sm-block
+          "
+        >
+          <div class="container">
+            <img src="../assets/images/p3.png" class="profile-img" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="container-lg">
+      <p class="fs-2 text-center fw-bold about pt-4">ABOUT ME</p>
+
+      <div class="row pt-3 justify-content-center">
+        <div class="col-12 container body col-md-5 col-lg-6 pt-4">
+          <img
+            src="../assets/images/profilem-removebg-preview.jpg"
+            class="img-fluid"
+            alt="..."
+          />
+        </div>
+        <div class="col-12 col-md-5 col-lg-6 pt-4">
+          <div class="lh-1 d-none d-md-block">
+            <p class="fs-3 fw-bold">UI/UX Designer & Web Developer</p>
+          </div>
+          <div class="container lh-1 text-center d-block d-sm-none">
+            <p class="fs-3 fw-bold">UI/UX Designer & Web Developer</p>
+          </div>
+          <p class="fs-5 d-none d-md-block">
+            I am a Frontend developer mostly working with HTML, CSS, JavaScript
+            and Vue Js Framework for almost two years. I have learn, done
+            internship and actually working for Vinsigt technologies....
+          </p>
+          <p class="fs-5 text-center d-block d-sm-none">
+            I am a Frontend developer mostly working with HTML, CSS, JavaScript
+            and Vue Js Framework for almost two years. I have learn, done
+            internship and actually working for Vinsigt technologies....
+          </p>
+          <div class="row">
+            <div class="col-12 col-md-6 col-lg-6">
+              <p class="fs-6">
+                <span class="fw-bold">Name:</span> TCHOUANFE FEZE BORIS
+              </p>
+              <p class="fs-6"><span class="fw-bold">Degree:</span> ...</p>
+              <p class="fs-6">
+                <span class="fw-bold">Phone:</span> +237 690 166 974
+              </p>
+              <p class="fs-6">
+                <span class="fw-bold">Adress:</span> Yaounde Cameroon
+              </p>
+              <div class="row">
+                <div class="col-4 d-none d-md-block">
+                  <div class="btn btn-sm btn-primary">Hire me</div>
+                </div>
+                <div class="col-5 d-none d-md-block">
+                  <div class="btn btn-sm btn-outline-primary">learn more</div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6">
+              <p class="fs-6">
+                <span class="fw-bold">Birthday:</span> 19 November 1997
+              </p>
+              <p class="fs-6"><span class="fw-bold">Degree:</span> ...</p>
+              <p class="fs-6">
+                <span class="fw-bold">Experience:</span> Two years
+              </p>
+              <p class="fs-6">
+                <span class="fw-bold">Email:</span> borisfeze9@gmail.com
+              </p>
+              <div class="row">
+                <div class="col-4 d-block d-sm-none">
+                  <div class="btn btn-sm btn-primary">Hire me</div>
+                </div>
+                <div class="col-5 d-block d-sm-none">
+                  <div class="btn btn-sm btn-outline-primary">learn more</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="container-lg my-3">
+      <p class="fs-2 text-center fw-bold about pt-4">My Skills</p>
+      <div class="row justify-content-center">
+        <div class="col-16 col-md-6 col-lg-6">
+          <div class="progress my-4">
+            <div
+              class="progress-bar bg-success"
+              role="progressbar"
+              style="width: 80%"
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              <span class="fw-bold"
+                >HTML &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;80%</span
+              >
+            </div>
+          </div>
+          <div class="progress my-4">
+            <div
+              class="progress-bar bg-warning"
+              role="progressbar"
+              style="width: 70%"
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              <span class="fw-bold"
+                >CSS &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;70%</span
+              >
+            </div>
+          </div>
+          <div class="progress my-4">
+            <div
+              class="progress-bar bg-info"
+              role="progressbar"
+              style="width: 65%"
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              <span class="fw-bold"
+                >JavaScript&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;65%</span
+              >
+            </div>
+          </div>
+        </div>
+        <div class="col-16 col-md-6 col-lg-6">
+          <div class="progress my-4">
+            <div
+              class="progress-bar bg-info"
+              role="progressbar"
+              style="width: 50%"
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              <span class="fw-bold"
+                >Flutter &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;50%</span
+              >
+            </div>
+          </div>
+          <div class="progress my-4">
+            <div
+              class="progress-bar bg-danger"
+              role="progressbar"
+              style="width: 60%"
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              <span class="fw-bold"
+                >WordPress &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;60%</span
+              >
+            </div>
+          </div>
+          <div class="progress my-4">
+            <div
+              class="progress-bar bg-primary"
+              role="progressbar"
+              style="width: 50%"
+              aria-valuenow="25"
+              aria-valuemin="0"
+              aria-valuemax="100"
+            >
+              <span class="fw-bold"
+                >Design&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;50%</span
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="container-lg">
+      <div class="row">
+        <div class="col-12 col-md-4 col-lg-4 mb-2">
+          <img src="../assets/images/css3.png" class="img-fluid" alt="..." />
+        </div>
+        <div class="col-12 col-md-4 col-lg-4 mb-2">
+          <img src="../assets/images/vuejs.png" class="img-fluid" alt="..." />
+        </div>
+        <div class="col-12 col-md-4 col-lg-4 mb-2">
+          <img src="../assets/images/flutter.jpg" class="img-fluid" alt="..." />
+        </div>
+      </div>
+      <div class="row my-3">
+        <div class="col-12 col-md-4 col-lg-4 mb-2">
+          <img src="../assets/images/js3.jpg" class="img-fluid" alt="..." />
+        </div>
+        <div class="col-12 col-md-4 col-lg-4 mb-2">
+          <img src="../assets/images/wp2.jpg" class="img-fluid" alt="..." />
+        </div>
+        <div class="col-12 col-md-4 col-lg-4 mb-2">
+          <img src="../assets/images/wdd.jpg" class="img-fluid" alt="..." />
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="container-lg">
+      <p class="fs-2 text-center fw-bold about pt-4">CONTACT ME</p>
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-6 my-2">
+          <input
+            type="text"
+            required
+            class="form-control"
+            placeholder="Your Name"
+            aria-label="Your name"
+          />
+          <input
+            type="text"
+            required
+            class="form-control my-2"
+            placeholder="Subject"
+            aria-label="Subject"
+          />
+          <div class="form-floating mt-2">
+            <textarea
+              class="form-control"
+              placeholder="Leave a comment here"
+              id="floatingTextarea2"
+              style="height: 100px"
+            ></textarea>
+            <label for="floatingTextarea2">Message</label>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-6 my-2">
+          <input
+            type="email "
+            required
+            class="form-control"
+            placeholder="Your Email"
+            aria-label="Your name"
+          />
+        </div>
+      </div>
+      <div class="container text-center mt-4">
+        <button class="btn btn-outline-primary btn">Send Message</button>
+      </div>
+    </div>
+  </section>
+  <section class="footer p-5 mt-5">
+    <div class="container-lg text-center">
+      <div class="row">
+        <div class="col">
+          <a href="https://wa.link/m7hbt2" target="_blank" class="p-2">
+            <!-- <box-icon type="logo" name="whatsapp"></box-icon
+          > -->
+            <i class="bx bxl-whatsapp bx-border-circle bx-sm"></i>
+          </a>
+
+          <a
+            target="_blank"
+            href="https://twitter.com/BTchouanfe?t=GNB9DSwDRdnnf0WVQD1GQw&s=08"
+            class="p-2"
+          >
+            <!-- <box-icon name="twitter" type="logo"></box-icon
+          > -->
+            <i class="bx bxl-twitter bx-border-circle bx-sm"></i
+          ></a>
+          <a
+            target="_blank"
+            href="https://www.youtube.com/channel/UCb7GIDR1w7VgFAKW5t9Owaw"
+            class="p-2"
+          >
+            <!-- <box-icon name="twitter" type="logo"></box-icon
+          > -->
+            <i class="bx bxl-youtube bx-border-circle bx-sm"></i
+          ></a>
+
+          <a
+            target="_blank"
+            href="https://www.facebook.com/profile.php?id=100008943550010"
+            class="p-2"
+          >
+            <!-- <box-icon class="box-icon" type="logo" name="facebook"></box-icon> -->
+            <i class="bx bxl-facebook bx-border-circle bx-sm"></i>
+          </a>
+          <!-- <i class="bx bxl-facebook-square bx-border-circle"></i> -->
+        </div>
+      </div>
+      <div class="container-lg text-center">
+        <p>
+          <small class="mb-5"
+            >Copyright@2022 Tchouanfe. All Rights Reserved</small
+          >
+        </p>
+
+        <p><small>built by Boris Tchouanfe</small></p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.top-section {
+  /* background: #136fff;
+  color: white; */
+  min-height: 100%;
+  background: linear-gradient(
+      0deg,
+      rgba(0, 89, 255, 0.3),
+      rgba(0, 89, 255, 0.3)
+    ),
+    url(https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60);
+  background-size: cover;
+  color: white;
+}
+.cv-btn {
+  background: white;
+  border-radius: 20px;
+  color: black;
+}
+.play-btn {
+  border-radius: 20px;
+  color: white;
+  border: 1px solid white;
+}
+.profile-img {
+  width: 400px;
+  height: 400px;
+}
+.smprofile-img {
+  width: 150px;
+  height: 150px;
+}
+.btn-container {
+  display: flex !important;
+  text-align: left;
+  margin: 0;
+}
+.btn-outline {
+  margin: 0px 10px;
+}
+.about {
+  color: #136fff;
+}
+.body img {
+  border-radius: 5px;
+}
+.footer {
+  background: #316ed1;
+}
+a i {
+  color: white !important;
+  margin-bottom: 30px;
+}
+.footer small {
+  color: #f0f0f0;
+  font-size: 0.7rem;
+}
+</style>
