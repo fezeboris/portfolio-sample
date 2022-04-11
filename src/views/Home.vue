56eaad4ec0f5a3dbb3d8a245cd51fd0d29ee2877
boris_tchouanfe <borisfeze9@gmail.com>
2022-04-11 01:54:31 +0100

    updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,14 +9,17 @@
           <div class="container my-2">
             <p class="text-xs-center fs-5">Hello, I'm called</p>
             <p class="display-5 text-xs-center fw-bold">BORIS TCHOUANFE</p>
-            <p class="text-xs-center fs-5">Web Developer</p>
-            <div class="btn-container">
+            <div class="wrapper">
+              <h5 class="text--static">I AM</h5>
+              <h5 class="text--dynamic"></h5>
+            </div>
+            <!-- <div class="btn-container">
               <button type="button" class="btn cv-btn">download cv</button>
 
               <button type="button" class="btn btn-outline play-btn">
                 play video
               </button>
-            </div>
+            </div> -->
           </div>
         </div>
         <div
@@ -58,15 +61,17 @@
           </p>
           <p class="fs-5 text-center d-block d-sm-none">
             I am a Frontend developer mostly working with HTML, CSS, JavaScript
-            and Vue Js Framework for almost two years. I have learn, done
-            internship and actually working for Vinsigt technologies....
+            and Vue Js Framework for almost two years. Currently employed a
+            Vinsigt technologies as Frontend web developer....
           </p>
           <div class="row">
             <div class="col-12 col-md-6 col-lg-6">
               <p class="fs-6">
                 <span class="fw-bold">Name:</span> TCHOUANFE FEZE BORIS
               </p>
-              <p class="fs-6"><span class="fw-bold">Degree:</span> ...</p>
+              <p class="fs-6">
+                <span class="fw-bold">Degree:</span> Autodidact
+              </p>
               <p class="fs-6">
                 <span class="fw-bold">Phone:</span> +237 690 166 974
               </p>
@@ -75,10 +80,19 @@
               </p>
               <div class="row">
                 <div class="col-4 d-none d-md-block">
-                  <div class="btn btn-sm btn-primary">Hire me</div>
+                  <div class="btn btn-sm btn-primary contact-btn">
+                    <a href="#contact">Hire me</a>
+                  </div>
                 </div>
                 <div class="col-5 d-none d-md-block">
-                  <div class="btn btn-sm btn-outline-primary">learn more</div>
+                  <div class="btn btn-sm btn-outline-primary contact-btn2">
+                    <a
+                      href="https://github.com/fezeboris"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      >learn more</a
+                    >
+                  </div>
                 </div>
               </div>
             </div>
@@ -86,19 +100,28 @@
               <p class="fs-6">
                 <span class="fw-bold">Birthday:</span> 19 November 1997
               </p>
-              <p class="fs-6"><span class="fw-bold">Degree:</span> ...</p>
+              <!-- <p class="fs-6"><span class="fw-bold">Degree:</span> ...</p> -->
               <p class="fs-6">
-                <span class="fw-bold">Experience:</span> Two years
+                <span class="fw-bold">Experience:</span> One years
               </p>
               <p class="fs-6">
                 <span class="fw-bold">Email:</span> borisfeze9@gmail.com
               </p>
               <div class="row">
                 <div class="col-4 d-block d-sm-none">
-                  <div class="btn btn-sm btn-primary">Hire me</div>
+                  <div class="btn btn-sm btn-primary contact-btn">
+                    <a href="#contact">Hire me</a>
+                  </div>
                 </div>
                 <div class="col-5 d-block d-sm-none">
-                  <div class="btn btn-sm btn-outline-primary">learn more</div>
+                  <div class="btn btn-sm btn-outline-primary contact-btn2">
+                    <a
+                      href="https://github.com/fezeboris"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      >learn more</a
+                    >
+                  </div>
                 </div>
               </div>
             </div>
@@ -203,7 +226,83 @@
     </div>
   </section>
   <section>
-    <div class="container-lg">
+    <div
+      id="demo"
+      class="carousel slide d-block d-sm-none"
+      data-ride="carousel"
+    >
+      <ul class="carousel-indicators">
+        <li data-target="#demo" data-slide-to="0" class="active"></li>
+        <li data-target="#demo" data-slide-to="1"></li>
+        <li data-target="#demo" data-slide-to="2"></li>
+        <li data-target="#demo" data-slide-to="3"></li>
+        <li data-target="#demo" data-slide-to="4"></li>
+        <li data-target="#demo" data-slide-to="5"></li>
+      </ul>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            src="../assets/images/css3.png"
+            alt="slide one"
+            width="1100"
+            height="500"
+          />
+          <div class="carousel-caption">CSS 3</div>
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/images/vuejs.png"
+            alt="slide two"
+            width="1100"
+            height="500"
+          />
+          <!-- <div class="carousel-caption">Vue js</div> -->
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/images/flutter.jpg"
+            alt="slide three"
+            width="1100"
+            height="500"
+          />
+          <!-- <div class="carousel-caption">Flutter</div> -->
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/images/js3.jpg"
+            alt="slide four"
+            height="500"
+            width="1100"
+          />
+          <div class="carousel-caption">JavaScript</div>
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/images/wp2.jpg"
+            alt="slide five"
+            width="1100"
+            height="500"
+          />
+          <!-- <div class="carousel-caption">WordPress</div> -->
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/images/wdd.jpg"
+            alt="slide five"
+            width="1100"
+            height="500"
+          />
+          <!-- <div class="carousel-caption">Web design</div> -->
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
+    </div>
+    <div class="container-lg d-none d-sm-block">
       <div class="row">
         <div class="col-12 col-md-4 col-lg-4 mb-2">
           <img src="../assets/images/css3.png" class="img-fluid" alt="..." />
@@ -230,46 +329,60 @@
   </section>
   <section>
     <div class="container-lg">
-      <p class="fs-2 text-center fw-bold about pt-4">CONTACT ME</p>
-      <div class="row">
-        <div class="col-12 col-md-6 col-lg-6 my-2">
-          <input
-            type="text"
-            required
-            class="form-control"
-            placeholder="Your Name"
-            aria-label="Your name"
-          />
-          <input
-            type="text"
-            required
-            class="form-control my-2"
-            placeholder="Subject"
-            aria-label="Subject"
-          />
-          <div class="form-floating mt-2">
-            <textarea
+      <form action="https://formspree.io/f/xpzbyqqd" method="POST">
+        <p class="fs-2 text-center fw-bold about pt-4" id="contact">
+          CONTACT ME
+        </p>
+        <div class="row">
+          <div class="col-12 col-md-6 col-lg-6 my-2">
+            <input
+              type="text"
+              required
               class="form-control"
-              placeholder="Leave a comment here"
-              id="floatingTextarea2"
-              style="height: 100px"
-            ></textarea>
-            <label for="floatingTextarea2">Message</label>
+              placeholder="Your Name"
+              name="Name"
+              v-model="name"
+              aria-label="Your name"
+            />
+            <input
+              type="text"
+              required
+              class="form-control my-2"
+              placeholder="Subject"
+              aria-label="Subject"
+              v-model="subject"
+              name="Subject"
+            />
+            <div class="form-floating mt-2">
+              <textarea
+                class="form-control"
+                placeholder="Leave a comment here"
+                id="floatingTextarea2"
+                style="height: 100px"
+                name="Message"
+                v-model="message"
+              ></textarea>
+              <label for="floatingTextarea2">Message</label>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-6 my-2">
+            <input
+              type="email "
+              v-model="email"
+              required
+              class="form-control"
+              placeholder="Your Email"
+              aria-label="Your Email"
+              name="Email"
+            />
           </div>
         </div>
-        <div class="col-12 col-md-6 col-lg-6 my-2">
-          <input
-            type="email "
-            required
-            class="form-control"
-            placeholder="Your Email"
-            aria-label="Your name"
-          />
+        <div class="container text-center mt-4">
+          <button class="btn btn-outline-primary btn" type="submit">
+            Send Message
+          </button>
         </div>
-      </div>
-      <div class="container text-center mt-4">
-        <button class="btn btn-outline-primary btn">Send Message</button>
-      </div>
+      </form>
     </div>
   </section>
   <section class="footer p-5 mt-5">
@@ -301,13 +414,9 @@
             <i class="bx bxl-youtube bx-border-circle bx-sm"></i
           ></a>
 
-          <a
-            target="_blank"
-            href="https://www.facebook.com/profile.php?id=100008943550010"
-            class="p-2"
-          >
+          <a target="_blank" href="https://github.com/fezeboris" class="p-2">
             <!-- <box-icon class="box-icon" type="logo" name="facebook"></box-icon> -->
-            <i class="bx bxl-facebook bx-border-circle bx-sm"></i>
+            <i class="bx bxl-github bx-border-circle bx-sm"></i>
           </a>
           <!-- <i class="bx bxl-facebook-square bx-border-circle"></i> -->
         </div>
@@ -315,7 +424,7 @@
       <div class="container-lg text-center">
         <p>
           <small class="mb-5"
-            >Copyright@2022 Tchouanfe. All Rights Reserved</small
+            >Copyright@{{ year }} Tchouanfe. All Rights Reserved</small
           >
         </p>
 
@@ -326,17 +435,101 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      year: "",
+
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    };
+  },
+  methods: {
+    onsubmit() {
+      (this.name = ""),
+        (this.email = ""),
+        (this.message = ""),
+        (this.subject = "");
+    },
+  },
+  mounted() {
+    const textElement = document.querySelector(".text--dynamic");
+    const wordsArray = [
+      "Web developer",
+      "graphic designer",
+      "Web designer",
+      "Flutter developer",
+      "Data scientist",
+    ];
+    let arrayLength = wordsArray.length;
+
+    let currentLetterCount = 0,
+      currentWordCount = 0,
+      currentText = [],
+      currentWord = "",
+      currentLetter = "",
+      reverse = false;
+
+    function typeWriting() {
+      if (currentWordCount <= arrayLength) {
+        if (currentWordCount == arrayLength) {
+          currentWordCount = 0;
+        }
+
+        let letterCount = wordsArray[currentWordCount].length;
+        currentWord = wordsArray[currentWordCount];
+
+        if (!reverse && currentLetterCount < letterCount) {
+          currentLetter = currentWord[currentLetterCount];
+          currentText.push(currentLetter);
+
+          textElement.textContent = currentText.join("");
+          currentLetterCount++;
+          // console.log(currentText, currentLetterCount, letterCount);
+        }
+
+        if (reverse && currentLetterCount <= letterCount) {
+          currentText.pop();
+          textElement.textContent = currentText.join("");
+          currentLetterCount--;
+          // console.log(currentText, currentLetterCount);
+        }
+
+        if (currentLetterCount == letterCount) {
+          reverse = true;
+        }
+
+        if (reverse && currentLetterCount == 0) {
+          currentText = [];
+          currentWordCount++;
+          reverse = false;
+        }
+      }
+      setTimeout(typeWriting, 300);
+    }
+
+    window.addEventListener("load", typeWriting);
+
+    let date = new Date();
+    this.year = date.getFullYear();
+    console.log(this.year);
+  },
+};
 </script>
 
 <style scoped>
+html {
+  scroll-behavior: smooth;
+}
 .top-section {
   /* background: #136fff;
   color: white; */
   min-height: 100%;
   background: linear-gradient(
       0deg,
-      rgba(0, 89, 255, 0.3),
+      rgba(0, 87, 249, 0.142),
       rgba(0, 89, 255, 0.3)
     ),
     url(https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60);
@@ -369,6 +562,18 @@ export default {};
 .btn-outline {
   margin: 0px 10px;
 }
+.contact-btn a {
+  text-decoration: none;
+  color: white;
+}
+.contact-btn2 a {
+  text-decoration: none;
+  color: #136fff;
+}
+.contact-btn2 a:hover {
+  text-decoration: none;
+  color: #ffff;
+}
 .about {
   color: #136fff;
 }
@@ -385,5 +590,57 @@ a i {
 .footer small {
   color: #f0f0f0;
   font-size: 0.7rem;
+}
+
+.wrapper {
+  height: 50px;
+  width: 100%;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-size: 0.8rem;
+  line-height: 50px;
+  display: flex;
+}
+
+.text--static {
+  padding-right: 3px;
+  text-align: left;
+  color: #000;
+}
+
+.text--dynamic {
+  color: #fff;
+  /* background: linear-gradient(
+    244deg,
+    #daed05 2%,
+    #04f6ea 49%,
+    #06f616 71%,
+    #daed05 100%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent; */
+  border-right: 2px solid;
+  height: 100%;
+  animation: blink 200ms ease infinite;
+}
+
+@keyframes blink {
+  0%,
+  100% {
+    border-right-color: transparent;
+  }
+
+  50% {
+    border-right-color: limegreen;
+  }
+}
+.carousel-inner img {
+  width: 100%;
+  height: 100%;
+}
+.carousel-caption {
+  color: #136fff;
+  font-weight: bold;
+  font-size: 1.5rem;
 }
 </style>
